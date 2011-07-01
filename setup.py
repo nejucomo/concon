2bef116f9167f20b04b824d589604d852fea9e09
Nathan Wilcox <nejucomo@gmail.com>
2011-07-01 09:21:18 -0700

    Add an untested setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'concon',
+      description = 'CONstrained CONtainers: immutable and append-only container subclasses and utilities.',
+      author = 'Nathan Wilcox <nejucomo@gmail.com>',
+      py_modules = ['concon'],
+      )
