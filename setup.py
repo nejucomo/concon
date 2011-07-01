f66bbba96fd9befddf774ff290eaf3e1f5d58444
Nathan Wilcox <nejucomo@gmail.com>
2011-07-01 09:23:51 -0700

    Add MIT license.

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ from distutils.core import setup
 setup(name = 'concon',
       description = 'CONstrained CONtainers: immutable and append-only container subclasses and utilities.',
       author = 'Nathan Wilcox <nejucomo@gmail.com>',
+      license = 'MIT (see LICENSE.txt)',
       py_modules = ['concon'],
       )
